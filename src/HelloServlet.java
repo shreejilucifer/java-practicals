031215b9b634929c93b6bcabef53adbeb1aad44f
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2019-02-08 07:59:40 +0530

    First Commit

---
+++ src/HelloServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+            res.getWriter().write( "Hello World" );
+
+    }
+}
